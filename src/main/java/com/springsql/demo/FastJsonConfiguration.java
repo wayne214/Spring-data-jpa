060f16de04779240634fa9da105a061fa026ff13
wayne <jun_wei_like1@163.com>
2019-01-21 15:23:57 +0800

    配置使用FastJson返回Json视图

---
+++ src/main/java/com/springsql/demo/FastJsonConfiguration.java
@@ -0,0 +1,48 @@
+package com.springsql.demo;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Configuration
+public class FastJsonConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 调用父类的配置
+//        super.configureMessageConverters(converters);
+        // 创建fastJson 消息转换器
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        // 创建配置类
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        // 修改配置返回内容的过滤
+        /**
+         * FastJson SerializerFeatures:
+         * WriteNullListAsEmpty  ：List字段如果为null,输出为[],而非null
+         * WriteNullStringAsEmpty ： 字符类型字段如果为null,输出为"",而非null
+         * DisableCircularReferenceDetect ：消除对同一对象循环引用的问题，默认为false（如果不配置有可能会进入死循环）
+         * WriteNullBooleanAsFalse：Boolean字段如果为null,输出为false,而非null
+         * WriteMapNullValue：是否输出值为null的字段,默认为false。
+         *
+         * */
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullListAsEmpty
+        );
+        // 处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+        // 在coverter中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        // 将fastjson 添加到视图消息转换器列表内
+        converters.add(fastConverter);
+    }
+}
